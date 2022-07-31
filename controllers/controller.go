d91f28354936c034f2d0414adadc87f23fe6bc02
vaibhavKLR <110060327+vaibhavKLR@users.noreply.github.com>
2022-07-31 23:25:25 +0530

    Update controller.go

--- controllers/controller.go
+++ controllers/controller.go
@@ -13,6 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	mongo_bson "gopkg.in/mgo.v2/bson"
 )
 
 const connectionString = "mongodb+srv://vaibhavs828:vaibhav@cluster0.ngtma.mongodb.net/?retryWrites=true&w=majority"
@@ -42,26 +43,75 @@ func init() {
 
 }
 
-func insertOneTodo(todo model.Todo) {
+func insertOneTodo(todo model.Todo) bson.M{
 	inserted, err := collection.InsertOne(context.Background(), todo)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("The Id is ", inserted.InsertedID)
+
+	var insertedTodo bson.M
+	cursor := collection.FindOne(context.Background(), bson.M{"_id": inserted.InsertedID}).Decode(&insertedTodo)
+	if cursor!=nil {
+		log.Fatal(cursor)
+	}
+	return insertedTodo
 }
 
 //updating a todo if completed
-func updateOneTodo(todoID string) {
+func updateOneTodo(todoID string) bool {
+
+	if !mongo_bson.IsObjectIdHex(todoID) {
+		return false
+	}
 	id, _ := primitive.ObjectIDFromHex(todoID)
 	filter := bson.M{"_id": id}
 	update := bson.M{"$set": bson.M{"completed": true}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Modified count is ", result.ModifiedCount)
+	return true
+}
+
+func updateAsUndone(todoID string) bool {
+	if !mongo_bson.IsObjectIdHex(todoID) {
+		return false
+	}
+	id, _ := primitive.ObjectIDFromHex(todoID)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"completed": false}}
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Modified count is ", result.ModifiedCount)
+	return true
+}
+
+//Edit the task
+func editOneTodo(todoID string, newTask model.Todo) bool{
+	id, _ := primitive.ObjectIDFromHex(todoID)
+
+	//If ID not found
+	if !mongo_bson.IsObjectIdHex(todoID) {
+		return false
+	}
+
+	filter := bson.M{"_id": id}
+	//The line below is working but not working for body
+	//update := bson.M{"$set": bson.M{"task": "Will be updated"}}
+	update := bson.M{"$set": bson.M{"task": newTask.Task}}
 
 	result, err := collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Modified count is ", result.ModifiedCount)
+	//fmt.Println("Task is ")
+	return true
 }
 
 //delete one todo
@@ -75,14 +125,22 @@ func deleteOneTodo(todoID string) {
 	fmt.Println("The delete count is ", deleteCount)
 }
 
-func getTodoById(todoID string) bson.M {
+func getTodoById(todoID string) (bool, bson.M) {
+
+	if !mongo_bson.IsObjectIdHex(todoID) {
+		return false, bson.M{}
+	}
+
 	id, _ := primitive.ObjectIDFromHex(todoID)
 	var todo bson.M
 	cursor := collection.FindOne(context.Background(), bson.M{"_id": id}).Decode(&todo)
+
+	//This terminates the connection
 	if cursor != nil {
 		log.Fatal(cursor)
 	}
-	return todo
+
+	return true, todo
 
 	//fmt.Println("Result is ", cursor)
 	// 	if err != nil {
@@ -125,7 +183,13 @@ func getAllTodos() []primitive.M {
 func GetTodoById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
 	params := mux.Vars(r)
-	todoById := getTodoById(params["id"])
+	//fmt.Println("Inside GetTOdoById")
+	err, todoById := getTodoById(params["id"])
+	if !err {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Wrong ID"))
+		return
+	}
 	json.NewEncoder(w).Encode(todoById)
 }
 
@@ -141,8 +205,8 @@ func CreateTodo(w http.ResponseWriter, r *http.Request) {
 
 	var todo model.Todo
 	_ = json.NewDecoder(r.Body).Decode(&todo)
-	insertOneTodo(todo)
-	json.NewEncoder(w).Encode(todo)
+	newTodo := insertOneTodo(todo)
+	json.NewEncoder(w).Encode(newTodo)
 }
 
 func MarkAsDone(w http.ResponseWriter, r *http.Request) {
@@ -150,8 +214,45 @@ func MarkAsDone(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
 
 	params := mux.Vars(r)
-	updateOneTodo(params["id"])
-	json.NewEncoder(w).Encode(params["id "])
+	err := updateOneTodo(params["id"])
+	//IF ID not found
+	if !err {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Wrong ID"))
+		return
+	}
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func MarkAsUndone(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
+	params := mux.Vars(r)
+	err := updateAsUndone(params["id"])
+	//IF ID not found
+	if !err {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Wrong ID"))
+		return
+	}
+	json.NewEncoder(w).Encode(params["id"])
+
+}
+
+func EditOneTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-Control-Allow-Methods", "PUT")
+
+	params := mux.Vars(r)
+	var updatedTaskTodo model.Todo
+	_ = json.NewDecoder(r.Body).Decode(&updatedTaskTodo)
+	err := editOneTodo(params["id"], updatedTaskTodo)
+	if !err {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Wrong ID"))
+		return
+	}
+	json.NewEncoder(w).Encode(updatedTaskTodo)
 }
 
 func DeleteATodo(w http.ResponseWriter, r *http.Request) {
